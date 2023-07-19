89ec59a44e54739ac55eaa859a4dac6729c2a7c0
Ahmed-Sawah <122928535+Ahmed-Sawah@users.noreply.github.com>
2023-07-19 22:41:45 +0300

    first commit

---
+++ frontend/src/components/NewsletterSignup.js
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { useFetcher } from "react-router-dom";
+import classes from "./NewsletterSignup.module.css";
+
+function NewsletterSignup() {
+  const fetcher = useFetcher();
+  const { data, state } = fetcher;
+
+  useEffect(() => {
+    if (state === "idle" && data && data.message) {
+      window.alert(data.message);
+    }
+  }, [data, state]);
+
+  return (
+    <fetcher.Form
+      method="post"
+      action="/newsletter"
+      className={classes.newsletter}
+    >
+      <input
+        type="email"
+        placeholder="Signup For Newsletter..."
+        aria-label="Signup For Newsletter"
+      />
+      <button>Signup</button>
+    </fetcher.Form>
+  );
+}
+
+export default NewsletterSignup;
